71cf7862a94f49d326fe49b1b861b9e8c84fb7de
Patrick Assoa Adou <kanian77@gmail.com>
2020-02-06 16:49:26 +0000

    feat: :star: Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { paramSchema } from './paramSchema'
+import { propertySchema } from './propertySchema'
+import { schema, validateConstructorParams } from './schema'
+
+export { schema, validateConstructorParams, propertySchema, paramSchema }
