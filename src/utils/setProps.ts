77da29fe3ddd68362779bb7749536b1d4a2963b7
Patrick Assoa Adou <patrick.assoa@payrix.com>
2022-10-12 18:17:16 +0000

    refactor: :recycle: setProps

--- src/utils/setProps.ts
+++ src/utils/setProps.ts
@@ -1,19 +1,22 @@
-import { NonFunctionKeys } from "utility-types"
+import { NonFunctionKeys } from "utility-types";
 
-
-export type PartialOfProperties<T extends object> = Partial<{ [P in NonFunctionKeys<T>]: T[P] }>
+export type PartialOfProperties<T extends object> = Partial<
+  { [P in NonFunctionKeys<T>]: T[P] }
+>;
 export function setProps<T extends object>(
-    o: T,
-    values: PartialOfProperties<T>
-  ): { values: PartialOfProperties<T>; errors: Error[] } {
-    let errors = []
-    for (let prop in values) {
-      try {
-          values[prop] !== undefined ? o[prop] = values[prop] : undefined
-      } catch (error) {
-        errors.push(error)
-        continue
+  o: T,
+  values: PartialOfProperties<T>
+): { values: PartialOfProperties<T>; errors: Error[] } {
+  let errors = [];
+  for (let prop in values) {
+    try {
+      if (values[prop] !== undefined) {
+        o[prop] = values[prop];
       }
+    } catch (error) {
+      errors.push(error);
+      continue;
     }
-    return { values, errors }
   }
+  return { values, errors };
+}
